f10952249c57cab02c807c7982f62cf481483bd8
Manoj <manoj@example.com>
2026-01-02 17:10:45 +0530

    commiting car2.cpp

---
+++ car2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Hello World" << endl;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Sum = " << a + b << endl;
+
+    return 0;
+}
